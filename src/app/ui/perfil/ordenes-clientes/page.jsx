7b4b80b7d656463094c547958905add7a4619555
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-04-26 15:46:55 -0500

    ✨ creacion cartas historial y registradas

--- src/app/ui/perfil/ordenes-clientes/page.jsx
+++ src/app/ui/perfil/ordenes-clientes/page.jsx
@@ -1,17 +1,15 @@
 'use client'
 import { useState, useEffect } from 'react'
-import Card from '@mui/material/Card'
+import Box from '@mui/material/Box'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
-import { Button, Grid } from '@mui/material'
+import { Grid } from '@mui/material'
 import { api } from '../../../../api/api'
 import dayjs from 'dayjs'
-
 export default function Page () {
   const [dataCard, setDataCard] = useState([])
-
   useEffect(() => {
-    const fetchData = async () => {
+    const traerDatos = async () => {
       try {
         const response = await api.get('/clientes/ordenes')
         setDataCard(response.data)
@@ -19,57 +17,57 @@ export default function Page () {
         console.log(error)
       }
     }
-
-    fetchData()
+    traerDatos()
   }, [])
-
   return (
-    <div className='p-5 mx-16'> {/* Agregamos padding al contenedor principal */}
-      {dataCard.length === 0 ? (
-        <div className='flex flex-col items-center justify-center h-full'>
-          <img src="https://i.ibb.co/r7tDx56/Black-Modern-Hacker-Cat-T-Shirt.png" className='w-52 sm:w-56 lg:w-56 mx-auto opacity-70' alt="No hay datos" />
-          <Typography variant="body1" color="textPrimary" className="text-center mt-4">
-            No hay datos disponibles.
-          </Typography>
-        </div>
-      ) : (
-        <div className='bg-gray-200'>
-          {dataCard.map((carta, index) => (
-            <div key={index} className='mb-6'> {/* Agregamos margen inferior a cada tarjeta */}
-              <Card className='rounded-lg shadow-lg border border-gray-200'>
-                <CardContent className='px-6 py-4'> {/* Ajustamos el padding */}
-                  <Grid container spacing={3}>
-                    <Grid item xs={12} sm={6}>
-                      <img className='max-w-xs mx-auto object-cover border rounded-md' src={carta.url_img} alt="Imagen producto" /> {/* Agregamos borde y ajustamos el tamaño de la imagen */}
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <div className='flex flex-col h-full'>
-                        <Typography variant="h5" gutterBottom>
-                          Número de orden: ANV-{carta.id_orden}
-                        </Typography>
-                        <Typography variant="body1" color="textSecondary" gutterBottom>
-                          Fecha de compra: {dayjs(carta.fecha_pedido).format('DD/MM/YYYY')}
-                        </Typography>
-                        <Typography variant="body1" gutterBottom>
-                          Valor: {carta.total}
-                        </Typography>
-                        <Typography variant="body1" gutterBottom>
-                          Estado: {carta.estado}
-                        </Typography>
-                        <div className='mt-auto'>
-                          <Button variant="contained" color="primary">
-                            Ver detalles
-                          </Button>
-                        </div>
-                      </div>
-                    </Grid>
-                  </Grid>
-                </CardContent>
-              </Card>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <>
+      <Box sx={{ minWidth: 700 }}>
+        {dataCard.map((carta, index) => (
+          <div key={index} className='bg-gray-100 rounded-lg border-slate-800  shadow  mt-20'>  <CardContent>
+          <Grid container columns={12}>
+            <Grid item xs={12} sm={6}>
+              <img className='max-w-xs' src={carta.url_img} alt="Imagen producto" />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <div className='pt-5'>
+                <div className='justify-center text-center mb-1.5  flex gap-2 '>
+                  <Typography sx={{ fontSize: 16 }} variant="h5">
+                    Numero de orden:
+                  </Typography>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                    {}
+                  </Typography>
+                </div>
+                <div className='justify-center text-center mb-1.5  flex gap-2 '>
+                  <Typography sx={{ fontSize: 16 }} variant="h5">
+                    Fecha compra:
+                  </Typography>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                    {dayjs(carta.fecha_pedido).format('DD/MM/YYYY')}
+                  </Typography>
+                </div>
+                <div className='justify-center text-center mb-1.5  flex gap-2 '>
+                  <Typography sx={{ fontSize: 16 }} variant="h5">
+                    Valor:
+                  </Typography>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                  {carta.total}
+                  </Typography>
+                </div>
+                <div className='justify-center text-center mb-1.5  flex gap-2 '>
+                  <Typography sx={{ fontSize: 16 }} variant="h5">
+                    Estado:
+                  </Typography>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                  {carta.estado}
+                  </Typography>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+        </CardContent></div>
+        ))}
+      </Box>
+    </>
   )
 }
