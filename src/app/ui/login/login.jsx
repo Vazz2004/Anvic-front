ac01786f35b843da1203003db28169ca93b1085c
Vazz2004 <samivazqueles@gmail.com>
2024-04-28 09:58:52 -0500

    💄Se agrega estilos en las tarjetas de los clientes
     🤡 🐛 El error del nav de perfil persoiste se intenta cambiar orden de los objetos html no se logra solucionar el error
    
    🔥 Se remueve datos de y manejador de errores de losgin se borra modal por que es codigom que no se  esta utilizando

--- src/app/ui/login/login.jsx
+++ src/app/ui/login/login.jsx
@@ -1,14 +1,12 @@
 'use client'
 import React, { useState } from 'react'
 import axios from 'axios'
-import Modal from 'react-modal'
-
+import Alerta from '../alertas/alert'
 const Login = () => {
   const [correo, setCorreo] = useState('')
   const [contrasena, setContrasena] = useState('')
-  const [modalIsOpen, setModalIsOpen] = useState(false)
   const [, setData] = useState()
-
+  const [errorRes, setErrorRes] = useState('')
   const handleCorreoChange = (event) => {
     setCorreo(event.target.value)
   }
@@ -30,26 +28,34 @@ const Login = () => {
       axios.post('http://localhost:5000/login', {
         userCorreo: correo,
         userPassword: contrasena
-      }).then((response) => {
-        if (response.data.user) {
-          const { user } = response.data // Desestructura el objeto 'user' de la respuesta
-          setData(user)
-          const [dataU] = user.usuario // Desestructura el array 'usuario' del objeto 'user'
-          console.log('CONEXIÓN EXITOSA', dataU)
-
-          localStorage.setItem('myToken', JSON.stringify(user)) // Convertir a cadena JSON antes de almacenar
-          console.log('id rol', dataU.rol_id)
-          if (dataU.rol_id === 1) {
-            window.location.href = '/perfil'
-          } else if (dataU.rol_id === 3) {
-            window.location.href = '/perfil/historial-compras-cliente'
-          }
-        }
-      }).catch(() => {
-        setModalIsOpen(true)
       })
+        .then((response) => {
+          if (response.data.user) {
+            const { user } = response.data
+            setData(user)
+            const [dataU] = user.usuario
+            console.log('CONEXIÓN EXITOSA', dataU)
+
+            localStorage.setItem('myToken', JSON.stringify(user))
+            console.log('id rol', dataU.rol_id)
+            if (dataU.rol_id === 1) {
+              window.location.href = '/perfil'
+            } else if (dataU.rol_id === 3) {
+              window.location.href = '/perfil/historial-compras-cliente'
+            }
+          }
+        })
+        .catch((error) => {
+          console.error(error) // Cambiado console.log a console.error
+          if (error.response && error.response.data && error.response.data.message) {
+            setErrorRes(error.response.data.message)
+          } else {
+            setErrorRes('Error al intentar iniciar sesión revisa el correo y la clave')
+          }
+        })
     }
   }
+
   return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -65,6 +71,10 @@ const Login = () => {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
+                {errorRes && (
+              <Alerta tipo="danger" mensaje={errorRes} />
+                )}
                     <form
                         className="space-y-6"
                         action="#"
@@ -142,20 +152,6 @@ const Login = () => {
                     </form>
                 </div>
             </div>
-
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={() => setModalIsOpen(false)}
-                style={{
-                  overlay: {
-                    zIndex: 1000
-                  }
-                }}
-            >
-
-                <h1>error de Inicio</h1>
-            </Modal>
-
         </>
   )
 }
--- src/app/ui/perfil/historial-compras-cliente/page.jsx
+++ src/app/ui/perfil/historial-compras-cliente/page.jsx
@@ -3,9 +3,10 @@ import { useState, useEffect } from 'react'
 import Box from '@mui/material/Box'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
-import { Grid } from '@mui/material'
+import { Button, Grid } from '@mui/material'
 import { api } from '../../../../api/api'
 import dayjs from 'dayjs'
+
 export default function Page () {
   const [dataCard, setDataCard] = useState([])
   useEffect(() => {
@@ -21,53 +22,66 @@ export default function Page () {
   }, [])
   return (
     <>
-      <Box sx={{ minWidth: 700 }}>
-        {dataCard.map((carta, index) => (
-          <div key={index} className='bg-gray-100 rounded-lg border-slate-800  shadow  mt-20'>  <CardContent>
-          <Grid container columns={12}>
-            <Grid item xs={12} sm={6}>
-              <img className='max-w-xs' src="https://firebasestorage.googleapis.com/v0/b/anvic-image.appspot.com/o/fe2e47eb-2938-4b1d-a185-e13ad44056ee?alt=media&token=b79a5c53-2f4a-4e51-a353-6fde972c17c6" alt="Imagen producto" />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <div className='pt-5'>
-                <div className='justify-center text-center mb-1.5  flex gap-2 '>
-                  <Typography sx={{ fontSize: 16 }} variant="h5">
-                    Numero de orden:
+      <Box sx={{ minWidth: 800, padding: '20px 0' }}>
+  {dataCard.map((carta, index) => (
+    <div key={index} className='bg-white rounded-lg shadow-lg mb-8'>
+      <CardContent>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={5}>
+            <img className='max-w-full mx-auto' src={carta.url_img} alt="Imagen del producto" />
+          </Grid>
+          <Grid item xs={12} sm={7}>
+            <div className='pt-5'>
+              <div className='justify-center text-center mb-4 flex flex-col'>
+                <Typography variant="h6" color="textPrimary" gutterBottom>
+                  Detalles del pedido
+                </Typography>
+                <div className='mb-2 flex items-center'>
+                  <Typography variant="subtitle1" color="textPrimary">
+                    Número de orden:
                   </Typography>
-                  <Typography sx={{ fontSize: 14 }} color="text.secondary">
-                    {}
+                  <Typography variant="subtitle1" color="textSecondary" className='ml-2'>
+                    {carta.numero_orden}
                   </Typography>
                 </div>
-                <div className='justify-center text-center mb-1.5  flex gap-2 '>
-                  <Typography sx={{ fontSize: 16 }} variant="h5">
-                    Fecha compra:
+                <div className='mb-2 flex items-center'>
+                  <Typography variant="subtitle1" color="textPrimary">
+                    Fecha de compra:
                   </Typography>
-                  <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                  <Typography variant="subtitle1" color="textSecondary" className='ml-2'>
                     {dayjs(carta.fecha_pedido).format('DD/MM/YYYY')}
                   </Typography>
                 </div>
-                <div className='justify-center text-center mb-1.5  flex gap-2 '>
-                  <Typography sx={{ fontSize: 16 }} variant="h5">
+                <div className='mb-2 flex items-center'>
+                  <Typography variant="subtitle1" color="textPrimary">
                     Valor:
                   </Typography>
-                  <Typography sx={{ fontSize: 14 }} color="text.secondary">
-                  {carta.precio}
+                  <Typography variant="subtitle1" color="textSecondary" className='ml-2'>
+                    ${carta.total}
                   </Typography>
                 </div>
-                <div className='justify-center text-center mb-1.5  flex gap-2 '>
-                  <Typography sx={{ fontSize: 16 }} variant="h5">
+                <div className='mb-2 flex items-center'>
+                  <Typography variant="subtitle1" color="textPrimary">
                     Estado:
                   </Typography>
-                  <Typography sx={{ fontSize: 14 }} color="text.secondary">
-                  {carta.estado}
+                  <Typography variant="subtitle1" color="textSecondary" className='ml-2'>
+                    {carta.estado}
                   </Typography>
                 </div>
               </div>
-            </Grid>
+              <div className='mt-4'>
+                <Button variant="contained" color="primary">
+                  Ver detalles
+                </Button>
+              </div>
+            </div>
           </Grid>
-        </CardContent></div>
-        ))}
-      </Box>
+        </Grid>
+      </CardContent>
+    </div>
+  ))}
+</Box>
+
     </>
   )
 }
