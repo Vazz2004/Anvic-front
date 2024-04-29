74debf2edc2b261b0e5e8320ebffb9fcf35df3a3
Vazz2004 <samivazqueles@gmail.com>
2024-04-29 17:33:27 -0500

    💄 Se gragan estilos para las margenes de la dash

--- src/app/ui/uiMother/vistaDash.jsx
+++ src/app/ui/uiMother/vistaDash.jsx
@@ -1,22 +1,21 @@
 export default function VistaDash ({ titulo, descripPage, content, boton }) {
   return (
-        <section className="w-full mx-20">
-            <div className="container mx-auto w-9/12 ">
-                <div className="flex flex-col md:flex-row items-center w-full justify-between gap-5 md:gap-8 px-4 md:px-6 max-w-6xl mx-auto">
-                    <div className="grid gap-1">
-                        <h1 className="text-3xl font-bold tracking-tight">{titulo}</h1>
-                        <p className="text-gray-500 dark:text-gray-400">{descripPage}</p>
-                    </div>
-                    <div className="w-full">
-                        {boton}
-                    </div>
-                </div>
-                <div className="flex justify-center mt-8 ">
-                    <div className=" w-full grid grid-cols-5 gap-4 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-5">
-                        {content}
-                    </div>
-                </div>
-            </div>
-        </section>
+<div className="container  px-4 md:px-6 w-full mt-56">
+    <div className="flex flex-col md:flex-row items-center justify-between gap-5 md:gap-8">
+        <div className="grid gap-1">
+            <h1 className="text-3xl font-bold tracking-tight">{titulo}</h1>
+            <p className="text-gray-500 dark:text-gray-400">{descripPage}</p>
+        </div>
+        <div className="w-full md:w-auto flex justify-end  ">
+            {boton}
+        </div>
+    </div>
+    <div className="flex justify-center mt-8">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-1">
+            {content}
+        </div>
+    </div>
+</div>
+
   )
 }
